7eff1267c307df0a6e1049213d2bc692b056e5a6
Jerzy9 <zbylutkonrad@gmail.com>
2020-10-26 18:25:11 +0100

    ServModel
    ServModel class with unit test

---
+++ src/test/java/com/hairdresser/booking/ModelTests.java
@@ -0,0 +1,30 @@
+package com.hairdresser.booking;
+
+import com.hairdresser.booking.model.Serv;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+import static org.junit.Assert.*;
+
+@SpringBootTest
+public class ModelTests {
+
+    private Serv serv;
+
+    @Test
+    public void createServ_fulfillWithDate_ReturnData() {
+        UUID id = UUID.randomUUID();
+        String name = "Haircut";
+        String description = "Simple haircut with scissors";
+        int time = 30*60*1000;      //30 min in milliseconds
+        float price  = (float) 25.99;
+
+        serv = new Serv(id, name, description, time, price);
+        assertEquals(serv.getId(), id);
+        assertEquals(serv.getName(), name);
+        assertEquals(serv.getDescription(), description);
+        assertEquals(serv.getTime(), time);
+        assertEquals((int) serv.getPrice(), (int) price);
+    }
+}

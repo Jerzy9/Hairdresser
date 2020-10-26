a60b7e19bd2b1c8fa5a1a65c9d6803a8bde1ddef
Jerzy9 <zbylutkonrad@gmail.com>
2020-10-26 16:54:30 +0100

    /hello end point
    http://localhost:8080/hello this is link, when you can check if app works properlly, your should see simple text Hello world

--- src/main/java/com/hairdresser/booking/api/HairServRestController.java
+++ src/main/java/com/hairdresser/booking/api/HairServRestController.java
@@ -1,9 +1,16 @@
 package com.hairdresser.booking.api;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HairServRestController {
 
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello world";
+    }
 }

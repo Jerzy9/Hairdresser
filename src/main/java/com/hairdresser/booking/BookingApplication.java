18098d1f6e1c6ba988d941e7ffa1981b11077250
Jerzy9 <zbylutkonrad@gmail.com>
2020-11-11 14:09:09 +0100

    in progres

--- src/main/java/com/hairdresser/booking/BookingApplication.java
+++ src/main/java/com/hairdresser/booking/BookingApplication.java
@@ -2,6 +2,8 @@ package com.hairdresser.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookingApplication {
@@ -9,5 +11,8 @@ public class BookingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookingApplication.class, args);
 	}
+
+//	@Bean
+//	public ServletRegistrationBean<>(GraphQLHtt)
 
 }
--- src/main/java/com/hairdresser/booking/graphql/GraphQLService.java
+++ src/main/java/com/hairdresser/booking/graphql/GraphQLService.java
@@ -1,13 +1,12 @@
 package com.hairdresser.booking.graphql;
 
+import com.coxautodev.graphql.tools.SchemaParserBuilder;
 import com.hairdresser.booking.graphql.datafetcher.AllHairstylesDataFetcher;
 import com.hairdresser.booking.graphql.datafetcher.HairstyleByIdDataFetcher;
+import com.hairdresser.booking.graphql.datafetcher.Query;
+import com.hairdresser.booking.service.HairstyleService;
 import graphql.GraphQL;
 import graphql.schema.GraphQLSchema;
-import graphql.schema.idl.RuntimeWiring;
-import graphql.schema.idl.SchemaGenerator;
-import graphql.schema.idl.SchemaParser;
-import graphql.schema.idl.TypeDefinitionRegistry;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,7 +14,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.File;
 import java.io.IOException;
 
 @Service
@@ -31,26 +29,38 @@ public class GraphQLService {
     private Resource resource;
 
     private GraphQL graphQL;
+    @Autowired
+    private HairstyleService hairstyleService;
 
     @PostConstruct
     private void loadSchema() throws IOException {
         //get the schema
-        File schemaFile = resource.getFile();
+//        File schemaFile = resource.getFile();
 
         //parse schema
-        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(schemaFile);
-        RuntimeWiring runtimeWiring = buildRuntimeWiring();
-        GraphQLSchema schema = new SchemaGenerator().makeExecutableSchema(typeRegistry, runtimeWiring);
+//        TypeDefinitionRegistry typeRegistry = new SchemaParser().parse(schemaFile);
+//        RuntimeWiring runtimeWiring = buildRuntimeWiring();
+        GraphQLSchema schema = buildSchema();
         graphQL = GraphQL.newGraphQL(schema).build();
     }
 
-    private RuntimeWiring buildRuntimeWiring() {
-        return RuntimeWiring.newRuntimeWiring()
-                .type("Query", typeWiring -> typeWiring
-                         .dataFetcher("getAllHairstyles", allHairstylesDataFetcher)
-                         .dataFetcher("getHairstyleById", hairstyleByIdDataFetcher))
-                .build();
+    private GraphQLSchema buildSchema() {
+        return new SchemaParserBuilder()
+                .file("graphql/main.graphqls")
+                .resolvers(
+                        new Query(hairstyleService)
+                )
+                .build()
+                .makeExecutableSchema();
     }
+
+//    private RuntimeWiring buildRuntimeWiring() {
+//        return RuntimeWiring.newRuntimeWiring()
+//                .type("Query", typeWiring -> typeWiring
+//                         .dataFetcher("getAllHairstyles", allHairstylesDataFetcher)
+//                         .dataFetcher("getHairstyleById", hairstyleByIdDataFetcher))
+//                .build();
+//    }
 
     public GraphQL getGraphQL() {
         return graphQL;
